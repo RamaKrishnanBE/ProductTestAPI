c791df80b9b8b5c0d20411940559374f77ad3e62
RamaKrishnanBE <g.ramakrishnan.be@gmail.com>
2023-01-26 22:15:19 +0530

    Made changes in Index method and GetAll Method of Product Controller.

--- ProductTestAPI/Controllers/ProductController.cs
+++ ProductTestAPI/Controllers/ProductController.cs
@@ -25,7 +25,7 @@ namespace ProductTestAPI.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            return Ok("Successsful");
+            return Ok("Index Method Execute Successfully");
         }
 
         [HttpGet]
@@ -33,8 +33,8 @@ namespace ProductTestAPI.Controllers
         {
             await UploadImageToAzureBlob();
 
-            return Ok();
-            //return Ok(await Mediator.Send(new GetAllProductsQuery()));
+           // return Ok();
+            return Ok(await Mediator.Send(new GetAllProductsQuery()));
         }
 
         [HttpGet("{id}")]
